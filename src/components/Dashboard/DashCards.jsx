ab3e085665782a5823834b92a721e6b84c2be413
TorresRojo <torresrojo@protonmail.com>
2020-02-19 12:52:48 -0600

    State Performance Cards

--- src/components/Dashboard/DashCards.jsx
+++ src/components/Dashboard/DashCards.jsx
@@ -1,53 +1,87 @@
   /** @jsx jsx */
   import React from 'react';
   import { Component } from 'react';
-  import { jsx, css, Global } from '@emotion/core';
-  import '../../css/Main.css';
-  import '../../css/Utilidades.css';
+  import { jsx, css } from '@emotion/core';
+ import Sendout from '../../img/icons/sendout.svg'
 
 
+const statsLayout = css `
+  width: 100%;
+  max-width: 1200px;
+  margin: auto;
+  display: flex;
+  background-color: #f2f2f2;
+    .l-performance{
+        width: calc(100% / 3);
+        margin: 20px;
+        height: 196px;
+        border-radius: 5px 5px 5px 50px;
+        background-color: White;
+        position: relative;
+        padding:  20px;
+        &-head{
+            p{}
+
+        }
+        img{
+            max-width: 98px;
+            width: 100%;
+            height: auto;
+            object-fit: contain;
+            position: absolute;
+            bottom: 10px;
+            left: 10px;
+        }
+        &--sendout{
+            border-right: 14px solid #50C9F5;
+        }
+        &--hotitem{
+            border-right: 14px solid #6950F5;
+        }
+        &--placements{
+            border-right: 14px solid #B0F461
+        }
+    }
+`;
+
+class StatsItemCard extends Component{
+    render (){
+        return(
+            <div className={this.props.className}>
+                <div className="l-performance-head">
+                    <p className="l-performance-head--title u-Roboto22 u-margin-0 u-text-700">{this.props.PerformanceHead}</p>
+                    <p className="l-performance-head--status u-Roboto16 u-margin-0 u-text-300">{this.props.PerformanceStatus}</p>
+                </div>
+                <img src={this.props.IconPerformance} alt=""/>
+            </div>
+        );
+    }
+}
 
 class CardStats extends Component{
     
     render(){
         return(
             <React.Fragment>
-                <Global styles={css`
-                    .l-stats{
-                        display: flex;
-                        max-width: 1000px;
-                        width: 100%;
-                        margin: auto;
-                        background-color: tan;
-                        &-card{
-                            width: calc(100%/3);
-                            height: 196px;
-                            box-shadow: 0px 3px 6px #00000007;
-                            border-radius: 5px 5px 5px 50px;
-                            background-color: #f2f2f2;
-                            margin: 20px;
-                            &--hotItem{
-
-                            }
-                            &--placements{
-
-                            }
-                            &--sendouts{
-
-                            }
-                        }
-                    }
-                `}/>
-                <div className="l-stats">
-                    <div className="l-stats-card l-stats-card--hotItem">
-
-                    </div>
-                    <div className="l-stats-card l-stats-card--placements">
-
-                    </div>
-                    <div className="l-stats-card l-stats-card--sendouts">
-
-                    </div>
+                <div css={statsLayout}>
+                    <StatsItemCard 
+                    className="l-performance l-performance--sendout" 
+                    PerformanceHead="SENDOUTS" 
+                    IconPerformance={Sendout}
+                    PerformanceStatus="Total Inventory"
+                    />
+                    <StatsItemCard 
+                    className="l-performance l-performance--hotitem" 
+                    PerformanceHead="HOT ITEMS" 
+                     IconPerformance={Sendout}
+                     PerformanceStatus="Total Inventory"
+                     />
+                    <StatsItemCard 
+                    className="l-performance l-performance--placements" 
+                    PerformanceHead="PLACEMENTS" 
+                     IconPerformance={Sendout}
+                     PerformanceStatus="Total Inventory"
+                     />
                 </div>
             </React.Fragment>
         );
