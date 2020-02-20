a2f63565363bb38b168a34bf70b0b71efaa9e506
TorresRojo <torresrojo@protonmail.com>
2020-02-20 16:19:08 -0600

    material inputs

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -6,29 +6,31 @@
   import dots from '../img/dots.svg';
   import doted from '../img/dots.svg';
   import gpac from '../img/GPAC-logo-GB.png';
-  import {
-    withStyles
-  } from "@material-ui/core/styles";
+  import {withStyles} from "@material-ui/core/styles";
 import TextField from '@material-ui/core/TextField';
   
 const CssTextField = withStyles({
     root: {
+      margin: "10px 0",
       "& label.Mui-focused": {
-        color: "green"
+        color: "#1A1C21",
+        backgroundColor: "#eceef2",
+        padding: "0 15px 0 3px",
       },
       "& .MuiInput-underline:after": {
-        borderBottomColor: "green"
+        borderBottomColor: "#1A1C21"
       },
       "& .MuiOutlinedInput-root": {
         "& fieldset": {
-          borderColor: "navy",
           borderRadius: "30px"
         },
         "&:hover fieldset": {
-          borderColor: "yellow"
+          borderColor: "#1A1C21",
+          borderWidth: "1px"
         },
         "&.Mui-focused fieldset": {
-          borderColor: "green"
+          borderColor: "#1A1C21",
+          borderWidth: "1px"
         }
       }
     }
@@ -94,10 +96,10 @@ const CssTextField = withStyles({
               }
               
               input{
-                  background: #FFFFFF 0% 0% no-repeat padding-box;
+                  background: #ECEEF2 0% 0% no-repeat padding-box;
                   max-width: 376px;
                   width: 100%;
-                  height: 60px;
+                  height: 54px;
                   padding: 0 25px;
                   border: none;
                   border-radius: 30px;
@@ -187,9 +189,19 @@ function BasicTextFields() {
     const classes = withStyles();
   
     return (
-      <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="outlined-basic" label="E-mail" variant="outlined" />
-        <TextField id="outlined-basic" label="Password" variant="outlined" />
+      <form className={classes.root} noValidate autoComplete="on">
+      <CssTextField
+        className={classes.margin}
+        label="E-mail"
+        variant="outlined"
+        id="custom-css-outlined-input"
+      />
+      <CssTextField
+        className={classes.margin}
+        label="Password"
+        variant="outlined"
+        id="custom-css-outlined-input"
+      />
       </form>
     );
   }
