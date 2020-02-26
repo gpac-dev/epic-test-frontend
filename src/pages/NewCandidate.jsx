9c9e34d9a22b25cc18dfef25961b2c717c8e7b60
TorresRojo <torresrojo@protonmail.com>
2020-02-26 16:13:35 -0600

    set inputs calc considering 100% width

--- src/pages/NewCandidate.jsx
+++ src/pages/NewCandidate.jsx
@@ -32,10 +32,13 @@ const HeaderTitle = styled.h1`
 
 `
 const InputContainer = css`
-    max-width: 280px;
+    max-width: calc(100% / 4 - 20px);
     width: 100%;
+    min-width: 250px;
     align-self: center;
     border-radius: 30px;
+    margin: 10px;
+    label: containerInput;
     .PrivateNotchedOutline-root-166 {
         border-radius: 30px;
        }
@@ -135,7 +138,13 @@ class CandidateNew extends Component {
                                         </div>
                                         <div css={InputContainer}>
                                             <InputFill LabelTag="Last Name"> </InputFill>
-                                        </div>  
+                                        </div>
+                                        <div css={InputContainer}>
+                                            <ComboBox LabelSelect='Industry'></ComboBox>
+                                        </div>
+                                        <div css={InputContainer}>
+                                            <ComboBox LabelSelect='Functional Title'></ComboBox>
+                                        </div>      
                                         <div css={InputContainer}>
                                             <InputFill LabelTag="Title"> </InputFill>
                                         </div>  
@@ -144,19 +153,24 @@ class CandidateNew extends Component {
                                         </div>  
                                         <div css={InputContainer}>
                                             <InputFill LabelTag="Email"> </InputFill>
-                                        </div>  
+                                        </div>
+                                        <div css={InputContainer}>
+                                            <ComboBox LabelSelect='State'></ComboBox>
+                                        </div>
+                                        <div css={InputContainer}>
+                                            <ComboBox LabelSelect='City'></ComboBox>
+                                        </div>   
                                         <div css={InputContainer}>
                                             <InputFill LabelTag="Zip"> </InputFill>
-                                        </div>  
+                                        </div> 
+                                        <div css={InputContainer}>
+                                            <ComboBox LabelSelect='Source'></ComboBox>
+                                        </div> 
                                         <div css={InputContainer}>
                                             <InputFill LabelTag="Source URL"> </InputFill>
                                         </div>
-                                        <div css={InputContainer}>
-                                            <ComboBox LabelSelect='E-mail'></ComboBox>
-                                        </div> 
-                                        <div css={InputContainer}>
-                                            <ComboBox LabelSelect='E-mail'></ComboBox>
-                                        </div>         
+                                     
+                                               
                                         </div>
                             </FormBox>
                         </InnerLayout>
